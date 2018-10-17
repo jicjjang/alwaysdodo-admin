1b65cdae75c95826d291de9408739f8bdc5beda1
Changwan Jun <im@wani.kr>
2018-10-17 17:26:01 +0900

    수정페이지에도 파일 업로드 추가

--- pages/admin/registries/_id.vue
+++ pages/admin/registries/_id.vue
@@ -8,7 +8,18 @@
       <el-input v-model="inputName" type="text" ref="inputName" />
     </el-form-item>
     <el-form-item label="Value">
-      <el-input :autosize="{minRows: 2}" v-model="inputValue" type="textarea" ref="inputValue" />
+      <el-input
+        type="textarea"
+        ref="inputValue"
+        :class="{'is-drag-enter': isDragEnter}"
+        :autosize="{minRows: 5}"
+        v-model="inputValue"
+        @dragenter.native="isDragEnter = true"
+        @dragover.native="isDragEnter = true"
+        @dragleave.native="isDragEnter = false"
+        @dragend.native="isDragEnter = false"
+        @drop.native.prevent="onDrop"
+      />
     </el-form-item>
     <el-form-item>
       <div class="align-justify">
@@ -35,9 +46,20 @@ export default {
     return {
       inputName: registry.name,
       inputValue,
+      isDragEnter: false,
     }
   },
   methods: {
+    async onDrop(e) {
+      const files = e.dataTransfer.files
+      if (files.length === 0) {
+        return
+      }
+      const form = new FormData()
+      form.append("attachment", files[0])
+      const response = await this.$axios.post("/attachments", form)
+      this.inputValue = `${this.inputValue || ""}${response.data.attachment.path}`
+    },
     async updateRegistry() {
       if (!this.inputName) {
         alert("Name을 입력해주세요.")
